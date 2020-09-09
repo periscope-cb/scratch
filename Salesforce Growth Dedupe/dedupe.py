2b6b2c1bd3fd52739e380aae479fc4096d9281a7
Kyle Cheung <kyle.c@clearbanc.com>
2020-09-09 18:27:28 -0400

    files

---
+++ Salesforce Growth Dedupe/dedupe.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df_sfdc = pd.read_csv('salesforce.csv')
+df = pd.read_csv('dedupe.csv')
+
+df['in_sfdc'] = df['website'].isin(df_sfdc['WEBSITE'])
+
+df.to_csv('dedupe_output.csv')
